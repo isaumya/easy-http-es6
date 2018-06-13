53224a80eba6e02fc0cba416b388e3bb09aaffe3
Saumya Majumder <saumya0305@gmail.com>
2018-06-13 14:03:14 +0530

    Updated app.js files and callbacks

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const http = new EasyHTTP();
 
 // Test the GET request
-// http.get('https://jsonplaceholder.typicode.com/users')
-// 		.then( data => document.getElementById('output').innerHTML = JSON.stringify(data) )
-// 		.catch( err => document.getElementById('output').innerHTML = err );
+http.get('https://jsonplaceholder.typicode.com/users')
+		.then( data => document.getElementById('output').innerHTML = JSON.stringify(data) )
+		.catch( err => document.getElementById('output').innerHTML = err );
 
 // Test the POST request
 /*const user = {
@@ -29,6 +29,6 @@ http.put('https://jsonplaceholder.typicode.com/users/10', updatedDetails)
 		.catch( err => document.getElementById('output').innerHTML = err );*/
 
 // Test the DELETE request
-http.delete('https://jsonplaceholder.typicode.com/users/110')
+/*http.delete('https://jsonplaceholder.typicode.com/users/10')
 		.then( response => document.getElementById('output').innerHTML = response )
-		.catch( err => document.getElementById('output').innerHTML = err );
+		.catch( err => document.getElementById('output').innerHTML = err );*/
